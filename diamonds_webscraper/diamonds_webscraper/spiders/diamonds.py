cfe21ec7cc79ea7546d03681603fc2612841fb7f
Siphu Langeni, MS <szlangeni@gmail.com>
2021-02-03 00:20:04 -0500

    Testing scrapy crawler

--- diamonds_webscraper/diamonds_webscraper/spiders/diamonds.py
+++ diamonds_webscraper/diamonds_webscraper/spiders/diamonds.py
@@ -6,20 +6,21 @@ import pandas
 
 class DiamondsSpider(scrapy.Spider):
 
-    def __init__(self):
+    def __init__(self, page_num=1, min_carat=0.25, max_carat=0.27):
 
         self.page_num = page_num
         self.min_carat = min_carat
         self.max_carat = max_carat
 
     name = 'diamonds'
-    allowed_domains = ['brilliantearth.com/loose-diamonds/list/']
+    allowed_domains = ['brilliantearth.com/']
     start_urls = [
         f'brilliantearth.com/loose-diamonds/list/?shapes=Round&min_carat={self.min_carat}&max_carat={self.max_carat}&page={self.page_num}'
     ]
 
-
     def parse(self, response):
+        rel_path = f'?shapes=Round&min_carat={self.min_carat}&max_carat={self.max_carat}&page={self.page_num}'
+        full_path = start_urls + rel_path
         data = json.loads(response.text)
         for diamond in data['diamonds']:
             yield {
@@ -31,17 +32,17 @@ class DiamondsSpider(scrapy.Spider):
                 'price': diamond['price']
             }
         
-        total_pages = ceil(data['total_count'] / 20)
-        next_page_number = data['page'] + 1
-        full_path = data['path']
-        min_carat = re.findall('(?<=min_carat=)\w+\.\w+', full_path.lower())[0]
-        max_carat = re.findall('(?<=max_carat=)\w+\.\w+', full_path.lower())[0]
-        next_min_carat = float(min_carat) + 0.1
-        next_max_carat = float(max_carat) + 0.1
+        # total_pages = ceil(data['total_count'] / 20)
+        # next_page_number = data['page'] + 1
+        # full_path = data['path']
+        # min_carat = re.findall('(?<=min_carat=)\w+\.\w+', full_path.lower())[0]
+        # max_carat = re.findall('(?<=max_carat=)\w+\.\w+', full_path.lower())[0]
+        # next_min_carat = float(min_carat) + 0.1
+        # next_max_carat = float(max_carat) + 0.1
     
-        if total_pages >= next_page_number:
-            next_page_url = f'https://www.brilliantearth.com/loose-diamonds/list/?shapes=Round&min_carat={min_carat}&max_carat={max_carat}&page={next_page_number}'
-        else:
-            next_page_url = f'https://www.brilliantearth.com/loose-diamonds/list/?shapes=Round&min_carat={next_min_carat:.2f}&max_carat={next_max_carat:.2f}&page=1'
+        # if total_pages >= next_page_number:
+        #     next_page_url = f'https://www.brilliantearth.com/loose-diamonds/list/?shapes=Round&min_carat={min_carat}&max_carat={max_carat}&page={next_page_number}'
+        # else:
+        #     next_page_url = f'https://www.brilliantearth.com/loose-diamonds/list/?shapes=Round&min_carat={next_min_carat:.2f}&max_carat={next_max_carat:.2f}&page=1'
         
         yield scrapy.Request(next_page_url, callback=self.parse)
