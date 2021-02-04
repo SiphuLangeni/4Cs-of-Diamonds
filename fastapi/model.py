e7113cbb4567ab9635696911b10d81f4ef6fda05
Siphu Langeni, MS <szlangeni@gmail.com>
2021-02-04 05:19:16 -0500

    Remove preprocessing from model

--- fastapi/model.py
+++ fastapi/model.py
@@ -28,37 +28,10 @@ class DiamondModel:
             joblib.dump(self.model, self.model_fname)
 
     
-    def pre_process(self):
+    def split_data(self):
         
-        df = self.df.drop_duplicates(subset=['upc'], keep='first', ignore_index=True)
-        
-        # Label encode categorical data
-        df.cut = df.cut.map(
-            {'Fair': 0, 'Good': 1, 'Very Good': 2, 'Ideal': 3, 'Super Ideal': 4}
-        )
-
-        df.colour = df.colour.map(
-            {'J': 0, 'I': 1, 'H': 2, 'G': 3, 'F': 4, 'E': 5, 'D': 6}
-        )
-
-        df.clarity = df.clarity.map(
-            {'SI2': 0, 'SI1': 1, 'VS2': 2, 'VS1': 3, 'VVS2': 4, 'VVS1':5, 'IF': 6, 'FL': 7}
-        )
-
-        df = df[['cut', 'colour', 'clarity', 'carat', 'price']].copy()
-
-        # Remove outliers
-        Q1 = df.price.quantile(0.25)
-        Q2 = df.price.quantile(0.5)
-        Q3 = df.price.quantile(0.75)
-        IQR = Q3 - Q1
-        lower_limit = Q1 - (1.5 * IQR)
-        upper_limit = Q3 + (1.5 * IQR)
-        df = df[(df.price >= lower_limit) & (df.price <= upper_limit)].reset_index(drop=True)
-        
-        # Split dataset
-        X = df.drop(['price'], axis=1)
-        y = df.price
+        X = self.df[['cut', 'colour', 'clarity', 'carat']].copy()
+        y = self.df.price
 
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.2, random_state=0
@@ -68,10 +41,10 @@ class DiamondModel:
     
     def train_model(self):
         
-        X_train, _, y_train, _ = self.pre_process()
+        X_train, _, y_train, _ = self.split_data()
         
         rf = RandomForestRegressor(
-            n_estimators=50,
+            n_estimators=60,
             bootstrap=True,
             random_state=0
         )
@@ -81,7 +54,7 @@ class DiamondModel:
 
     def get_metrics(self):
         
-        _, X_test, _, y_test = self.pre_process()
+        _, X_test, _, y_test = self.split_data()
 
         y_pred = self.model.predict(X_test)
     
